05f288b391f8670ab3cf793d99ca2057b30d97b6
igada <iga.dai0226@gmail.com>
2026-01-14 10:31:18 +0900

    flutterプロジェクトフォルダ追加

---
+++ flutter2025_numberguessinggame/lib/GamePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class GamePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ホーム")),
+      body: Center(
+        child: Container(
+          color: Colors.red
+        ),
+      ),
+    );
+  }
+}
